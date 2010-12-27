9bfe82848757c63ff3f316673cc3eee29150acb4
Anthony <antscape@gmail.com>
2010-12-28 01:17:04 +1100

    added math parser that will compile functions that plot relations

---
+++ rewrite/main.js
@@ -0,0 +1,111 @@
+
+var graphs=[];
+function getlatexpart(match, submatch)
+{
+  if(submatch == ",")
+	return "";
+  if(latexchars[submatch] !== undefined)
+	return latexchars[submatch];
+  return submatch;
+}
+
+
+function track(event) {
+	if (window.pageTracker) {
+		setTimeout(function () {
+			pageTracker._trackEvent("Graph", event)
+		}, 20)
+	}
+};
+
+
+
+
+function safeeval(z) {
+	//use safeeval instead of eval()
+	var naughty = "eval,document,window,location,cookie,alert,comfirm,prompt,this,parent,child,xml,xmlhttp,clip,draw,getfunction,get2dfunction,extrafunc,calcnextframe,nextframe,canvas".split(",");
+	for (var nau_g = 0; nau_g < naughty.length; nau_g++) {
+		if (z.indexOf(naughty[nau_g]) != -1) {
+			throw ("Unsafe Code: " + naughty[nau_g]);
+			return;
+		}
+	}
+	return eval(z);
+}
+
+
+
+randfuncs = "x^2	f'\\left(x\\right)-1	2e^{-x}	2x+3	\\lambda=3	e^{-\\lambda*x}	\\left(0.5,0.5\\right)	\\sum_{n=1}^{\\infinity}\\frac{\\sin\\left(nx\\right)}n	\\prod_{1}^{4}x-n	m:H_2SO_4	\\left|x^2-4\\right|+2	\\frac1x	x^{-2}	x!	\\ln x	\\sum_{n=1}^{\\infinity}\\frac{x^n}{n}	\\sin x	e^x:\\left[−2,2\\right]	\\tan\\left(x\\right)	\\left(x+2\\right)\\left(x-3\\right)^2	diff\\left(0,2,2x\\right)	\\left(x-2\\right)^2	\\sum_{n=1}^{\\infinity}\\frac{\\sin\\left(\\left(2n−1\\right)x\\right)}{2n−1}	\\prod_{n=1}^5\\left(x-n\\right)	\\sum_{n=0}^5n	x^x	\\Gamma\\left(x\\right)	\\frac{x!}{3!-x}	x%3	\\left(x>3\\right)?2x:-3	\\fact\\left(x\\right)	\\frac\\phi x	\\left(x>=0\\right)?m_e*G/\\left(r_e+100000x\\right)^2:undefined	g\\left[0\\right]'\\left(2x\\right)	g\\left[0\\right]\\left(x\\right)+1	\\sqrt x".split("	"); //1 tab - maybe it should be four spaces?
+
+var randfunc_index = 0;
+
+//Not actually random.
+function randfunc() {
+	return randfuncs[(randfunc_index++) % randfuncs.length];
+}
+
+
+
+
+
+
+
+
+//function management
+
+
+function delfunc(delete_button_node){
+	this_node=delete_button_node.parentNode;
+	index=0;
+
+	other_node=flist.firstChild;
+	while(other_node!=null){
+		if(other_node==this_node){
+			break;
+			index++;
+		}
+		other_node=other_node.nextSibling;
+	}
+	
+	g.splice(index,1);
+
+	colorss.push(colors_in_use.splice(index,1)[0]);
+	
+	flist.removeChild(this_node);
+	save();
+};
+
+function random_hash(){
+    var s="";
+    for(var i=0;i<20;i++){
+        s+=(~~(Math.random()*16)).toString(16);
+    }
+    return s;
+}
+var graph=function(n){
+	var t=compile(n || randfunc());
+	t.gid=random_hash();
+	t.color=app.ui.colors.free.pop();
+	t.node=app.ui.add(t);
+	return t;
+};
+
+app.version="GIT_VERSION";
+app.add=function(n){
+	graphs.push(new graph(n));
+}
+app.init=function (){
+	app.ui.init();
+	app.add();
+	if (window.location.hash != "") {
+		//index.html#y=x^2+2
+		window.shouldload = false;
+		if (window.location.hash[2] == "=") {
+			window.jsonfunc = window.location.hash.substring(3);
+		}else{
+			window.jsondata = unescape(window.location.hash.substring(6));
+		}
+	}
+};
+
+app.init();
---
+++ rewrite/ui.js
@@ -0,0 +1,584 @@
+Array.prototype.remove = function(elem) {  
+	var index = this.indexOf(elem)
+	if (index !== -1) {this.splice(index, 1)}
+}
+
+String.prototype.capitalize=function(){return this.charAt(0).toUpperCase()+this.slice(1);};
+
+var app={};
+app.ui=(function(){
+	
+	var webkit=/[Ww]eb[kK]it/.test(navigator.userAgent);
+	
+	var ctx;
+	var ptd,con,proto;
+	
+	var width,height,draw;
+
+	function resize(){
+	    width=window.innerWidth  || document.body.clientWidth;
+	    height=window.innerHeight|| document.body.clientHeight || 120;//120 for iframe
+
+	    canvas.width = width;
+	    canvas.height= height;
+	    ctx && draw();
+	}
+	//Mouse coordinates
+	var mx = 400;
+	var my = 300;
+
+	//Last mouse coordinates
+	var lmx=mx;
+	var lmy=my;
+
+	var drag;
+
+	var scalex = 128;
+	var scaley = scalex;//not always
+
+
+	//Visible region on screen
+	var boundleft = -10;
+	var boundright = 10;
+	var boundtop = 10;
+	var boundbottom = -10;
+
+	var gridsize;
+
+	//Location of canvas on screen. While dragging this changes.
+	var _cx = 0;
+	var _cy = 0;
+	
+	//camera
+	var cx = 0;
+	var cy = 0;
+	var cz = 10000;
+
+
+	function draw(){
+	
+	    e = Math.E;//they can be accidentially changed
+	    pi = Math.PI;
+
+	    if (!ctx) {
+	        return;
+	    }
+	    ctx.lineCap = "butt";
+
+	    ctx.clearRect(0, 0, width, height);
+
+	    //try{
+
+	    boundleft = cx / scalex;
+	    boundright = (width + cx) / scalex;
+	    boundbottom = -(height - cy) / scaley;
+	    boundtop = cy / scaley;
+
+
+	    gridsize = pow(2, 6 - Math.round(log(scalex) / log(2)));
+	    var overleft = gridsize * ~~ (boundleft / gridsize) - gridsize;
+	    var overright = gridsize * ~~ (boundright / gridsize) + gridsize;
+	    var overtop = gridsize * ~~ (boundtop / gridsize) + gridsize;
+	    var overbottom = gridsize * ~~ (boundbottom / gridsize) - gridsize;
+
+	    //Draw grid lines
+	
+		
+		ctx.lineWidth=2;
+	
+	
+	
+		ctx.beginPath();
+		ctx.move(0,0);
+		ctx.line(1,1);
+		ctx.stroke();
+		
+		
+					
+	    ctx.beginPath();
+	    ctx.move(overleft,0);
+	    ctx.line(overright,0);
+	    ctx.stroke();
+
+	    ctx.beginPath();
+	    ctx.move(0,overbottom);
+	    ctx.line(0,overtop);
+	    ctx.stroke();
+
+
+	    ctx.strokeStyle = "#888";
+
+	    ctx.lineWidth = 0.1;
+	    for (var x = overleft; x <= overright; x += gridsize / 4) {
+	        ctx.beginPath();
+	        ctx.move(x, overbottom);
+	        ctx.line(x, overtop);
+	        ctx.stroke();
+	    }
+	    for (var y = overbottom; y <= overtop; y += gridsize / 4) {
+	        ctx.beginPath();
+	        ctx.move(overleft, y);
+	        ctx.line(overright, y);
+	        ctx.stroke();
+	    }
+
+
+
+	   ctx.lineWidth = 0.4;
+		for (var x = overleft; x <= overright; x += gridsize) {
+			ctx.beginPath();
+			ctx.move(x, overbottom);
+			ctx.line(x, overtop);
+			ctx.stroke();
+		}
+
+	    for (var y = overbottom; y <= overtop; y += gridsize) {
+			ctx.beginPath();
+			ctx.move(overleft, y);
+			ctx.line(overright, y);
+			ctx.stroke();
+		}
+		ctx.beginPath();
+		ctx.move(0,0);
+		ctx.line(100,100);
+		ctx.stroke();
+
+
+	    //}catch(ex){}
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+	var drawwhiledrag_c=0;
+	function mousedown(e) {
+	    lmx=mx=e.x || e.pageX;
+	    lmy=my=e.y || e.pageY;
+	    drag = true;
+	    canvas.style.cursor = "url(grabbing.gif), grabbing";
+	    if (!drawwhiledrag_c) {
+	        setTimeout(drawwhiledrag, 1000);
+	        drawwhiledrag_c++;
+	    }
+	};
+
+	function mousemove(e) {
+	    e = e || window.event;
+	    if (e.x !== undefined) {
+	        mx = e.x;
+	        my = e.y;
+	    } else {
+	        mx = e.pageX;
+	        my = e.pageY;
+	    }
+	    if(drag){
+	        _cx += mx - lmx;
+	        _cy += my - lmy;
+			if(webkit){
+				canvas.style["-webkit-transform"]="translate("+_cx+"px,"+_cy+"px)";
+			}else{
+				canvas.style.left = _cx + "px";
+				canvas.style.top = _cy + "px";
+			}
+	
+	    }
+	    lmx = mx;
+	    lmy = my;
+	};
+	var scaleconst = 0.001;
+	if (/AppleWebKit/.test(navigator.userAgent)) {
+	    scaleconst = 0.0001;
+	}
+	if (/Firefox/.test(navigator.userAgent)) {
+	    scaleconst = 0.01;
+	}
+	if (/Opera/.test(navigator.userAgent)) {
+	    scaleconst = 0.03
+	}
+	if (!/Mac OS X/.test(navigator.userAgent)) {
+	    scaleconst = 0.1
+	}
+
+	function mousewheel(e){
+		e = e || window.event;
+	    if (e.x !== undefined) {
+	        mx = e.x;
+	        my = e.y;
+	    } else {
+	        mx = e.pageX;
+	        my = e.pageY;
+	    }
+		var ex=Math.exp(e.wheelDeltaY*scaleconst);
+	    scalex*=ex;
+	    scaley*=ex;
+		
+		/*
+		
+			nscalex/scalex=exp
+			mx =  scalex*px - cx
+		
+			(mx + cx)/scalex=px
+			∆cx =  nscalex*(mx + cx)/scalex - cx - mx
+			∆cx =  ex*(mx + cx) - cx - mx
+			
+			(cy-my)/scaley = py
+			
+			my +ex*(cy-my) - cy= ∆cy
+			
+			mx =  - cx
+			
+		*/
+		var dx=mx+cx;
+		var dy=my-cy;
+		if((dx*dx+dy*dy)>1000){
+			cx=ex*(mx+cx)-mx;
+			cy+=my+ex*(cy-my)-cy;
+		}
+	    draw();
+	}
+	function perform_translation(){
+	    cx-=_cx;
+	    cy+=_cy;
+	    _cx=_cy=0;
+		if(webkit){
+			canvas.style["-webkit-transform"]="translate(0px,0px)";
+		}else{
+			
+		    canvas.style.left = _cx + "px";
+		    canvas.style.top = _cy + "px";
+		}
+	}
+
+	function drawwhiledrag() {
+	    if (drag) {
+	        perform_translation();
+	        draw();
+	        setTimeout(drawwhiledrag, 1000);
+	    }else{
+	        drawwhiledrag_c--;
+	    }
+	}
+
+
+
+
+
+
+	function generateJSON(obj){
+			var w=document.createElement("ul");
+			w.className="json";
+			var mode=typeof obj;
+			if(obj===null){
+				mode="undefined";
+			}
+			if(mode=="function" && obj.length!=undefined && obj[0]!=undefined){
+				mode="object";
+			}
+			mode=mode.toString();
+			switch(mode){
+				case "number":
+
+					var fn=document.createElement("span");
+					fn.appendChild(document.createTextNode(obj));
+					w.appendChild(fn);
+					return w;
+
+					break;
+				case "string":
+					var fn=document.createElement("strong");
+					fn.appendChild(document.createTextNode("\""+obj+"\""));
+					w.appendChild(fn);
+					return w;
+
+				break;
+				case "boolean":
+					w.appendChild(document.createTextNode(obj));
+					return w;
+
+				break;
+				case "undefined":
+				case "function":
+					var fn=document.createElement("i");
+					if(obj===undefined){
+
+						fn.appendChild(document.createTextNode("undefined"));
+					}else if(obj===null){
+
+						fn.appendChild(document.createTextNode("null"));
+					}else{
+
+						fn.appendChild(document.createTextNode(obj.toString()));
+					}
+					w.appendChild(fn);
+					return w;
+				break;
+
+				case "object":
+				var found=false;
+
+	function do_loop(i){
+
+		var li=document.createElement("li");
+
+		var m2=typeof obj[i];
+		if(obj[i]===null || obj[i]===undefined){
+			m2="undefined";
+		}
+
+		switch(m2){
+
+			case "function":
+			case "object":
+
+				var b=document.createElement("b");
+
+				b.appendChild(document.createTextNode(i+": "));
+
+				var div=document.createElement("div");
+
+				div.appendChild(b);
+				div.appendChild(document.createTextNode((typeof(obj[i])).capitalize()));
+				li.appendChild(div);
+				li.obj=obj[i];
+
+				var children=document.createElement("div");
+				children.className="child";
+				li.appendChild(children);
+				li.done=false;
+				li.className="hide";
+
+				li.addEventListener("click",function(e){
+					e.stopPropagation();
+					if(this.className=="show"){
+						this.className="hide";
+						return;
+					}
+
+					this.className="show";
+					if(!this.done){
+						this.getElementsByClassName("child")[0].appendChild(generateJSON(this.obj));
+						this.done=true;
+					}
+
+					return false;
+				},false);
+
+				break;
+			default:
+
+				var b=document.createElement("b");
+				li.className="end";
+				b.appendChild(document.createTextNode(i+": "));
+
+				li.appendChild(b);
+				var str=obj[i];
+				if(m2=="undefined"){
+					var strong=document.createElement("i");
+					strong.appendChild(document.createTextNode(str));
+					li.appendChild(strong);
+				}else if(m2=="boolean"){
+					li.appendChild(document.createTextNode(str));
+				}else if(m2=="string"){
+					var strong=document.createElement("strong");
+					strong.appendChild(document.createTextNode("\""+str+"\""));
+					li.appendChild(strong);
+				}else if(m2=="number"){
+					var strong=document.createElement("span");
+					strong.appendChild(document.createTextNode(str));
+					li.appendChild(strong);
+				}else {
+					li.appendChild(document.createTextNode(str));
+				}
+
+
+
+		}
+		w.appendChild(li);
+	}
+	    for(i in obj){
+	        found=true;
+	        do_loop(i);
+	    }
+	    if(!found){
+	        if(obj.length!==undefined){
+	            for(var i=0;i<obj.length;i++){
+	                found=true;
+	                do_loop(obj[i]);
+	            }
+	            if(!found){
+	                w.appendChild(document.createTextNode(obj));
+	                return w;
+	            }
+	        }else{
+	            w.appendChild(document.createTextNode(obj));
+	            return w;
+	        }
+	    }
+	    break;
+	    default:
+
+
+	    }
+	return w;
+
+	}
+	
+	var ul;
+	
+	
+	var ui={
+	"add":function(n){
+		var li=proto.cloneNode(true);
+		
+		if(!ul){
+			ul=document.getElementById("graphs");
+		}
+		ul.appendChild(li);
+		var inputbox = li.getElementsByClassName("matheditor")[0];
+		inputbox.appendChild(document.createTextNode("x+3"));
+		$(inputbox).mathquill("editable");
+		$(inputbox).mathquill("redraw");
+		if(!n){
+            $(inputbox).trigger({ type: "keydown", ctrlKey: true, which: 65 });
+        }
+		
+		
+		//If loaded:
+		//  inputbox.focus();
+		return li;
+	},
+	"colors":{
+		"free":("#f08,#8f0,#80f,#f08,#880,#088,#808,#0ff,#f80,#f0f,#04f,#0a0,#f00,#07c".split(",")),
+		"used":[]
+	},
+	
+	"init":function(){
+		var fullscreen=window.parent.length?false:true;
+		(new Image()).src="grabbing.gif";
+		canvas=document.createElement("canvas");
+		if(fullscreen){
+			canvas.width=window.innerWidth;
+			canvas.height=window.innerHeight;
+		}
+		document.body.appendChild(canvas);
+		if(canvas.getContext){
+			ctx=canvas.getContext("2d");
+		}else{
+			alert("Failed!");
+		}
+		
+		//TODO: css
+		canvas.style.background="white";
+		canvas.style.cursor = "default";
+		canvas.style.position="fixed";
+		
+		
+		ptd=document.createElement("div");
+		ptd.id="ptd";
+		ptd.className="monospace";
+		ptd.appendChild(document.createTextNode("(0,0)"));
+		document.body.appendChild(ptd);
+		con=document.createElement("div");
+		con.id="con";
+		con.className="overlay";
+		con.style.display="none";
+		con.innerHTML="<div id=\"logt\" class=\"monospace\"><div>graph.tk v1.2 (<a href=\"https://github.com/aantthony/graph.tk/commit/"+app.version+"\">"+app.version+"</a>) &copy; 2010 Anthony<br /><br />License: <a href=\"http://www.gnu.org/licenses/lgpl.html\" title=\"GNU Lesser General Public License\" target=\"_blank\">GNU LGPL</a><br />Source code: <a href=\"http://github.com/aantthony/graph.tk\" target=\"_blank\">graph.tk on GitHub</a><br /><div style=\"font-family:sans-serif;font-size:x-small\">This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.<br /><br>Notes:<br>Sometimes x^n will not be x to the power of n, but x XOR n, which is the normal javascript meaning. I have made modifications that allow some easier equations. A dot after the expression means it was evaluated in a non-javascript-standard way. Use size() to change size.<br></div><br>Some cool things to type:<br> Fe, m:H_2O, g(0), g[1](0), en[26], M[26], symbol[26]</div></div><br><input type=\"text\" id=\"conin\" \">";
+		document.body.appendChild(con);
+		var conin=document.getElementById("conin");
+		conin.onkeydown=function(event){
+			if(event.which==13){
+				alert(conin.value);
+			}
+			if(event.which==38){
+				conin.value=last;
+			}
+		};
+
+		var funcs=document.createElement("div");
+		funcs.className="overlay";
+		funcs.id="funcs";
+		var _ul=document.createElement("ul");
+		_ul.id="graphs";
+		funcs.appendChild(_ul);
+		
+		var _proto=document.createElement("li");
+		var _proto_div=document.createElement("div");
+		_proto_div.className="b";
+		_proto_div.style.backgroundColor="#07c";
+		
+		var _proto_math=document.createElement("span");
+		_proto_math.className="matheditor";
+		var _proto_del=document.createElement("span");
+		_proto_del.className="delete";
+		_proto_del.onclick=function(e){
+			console.log(["del",e.target]);
+		};
+		
+		_proto.appendChild(_proto_div);
+		_proto.appendChild(_proto_math);
+		_proto.appendChild(_proto_del);
+		
+		
+		funcs.innerHTML+="<input type=\"button\" value=\"+\" onclick=\"graph.add()\"><a href=\"javascript:void(graph.showcon())\">Console</a><a href=\"javascript:void(tdiff())\">Diff Eq</a><a href=\"javascript:void(graph.screenshot())\">Screenshot</a><small id=\"nosave\"></small><div style=\"float:right\"><a href=\"http://graph.tk/about\" target=\"_blank\"><input type=\"button\" value=\"Info\" /></a>";
+		document.body.appendChild(funcs);
+		proto = _proto.cloneNode(true);
+		proto.removeAttribute("id");
+
+
+		window.onresize=resize;
+		canvas.onmousedown=mousedown;
+		document.body.addEventListener("mouseup",function(){drag=false;perform_translation();canvas.style.cursor = "default";draw()},false);
+		document.body.addEventListener("mousemove",mousemove,false);
+		   	   window.addEventListener("mousewheel",mousewheel,false);
+		document.body.removeChild(document.body.firstChild);
+			
+		//we may have to implement scaling if browsers don't work properly
+		ctx.move=function(px,py,pz){
+			return ctx.moveTo(scalex*px-cx,cy-scaley*py);
+			return;
+			if (!isNaN(py)) {
+				if (py > boundtop*4) {
+					ctx.moveTo(scalex*(px-cx), scaley*(cy-boundtop*4));
+					return;
+				} else if (py < boundbottom*4) {
+					ctx.moveTo(scalex*(px-cx), scaley*(cy-boundbottom*4));
+					return;
+				}
+				ctx.moveTo(scalex*(px+cx), scaley*(-cy-py));
+			}
+		};
+		ctx.line=function(px,py,pz){
+			return ctx.lineTo(scalex*px-cx,cy-scaley*py);
+			ctx.lineTo(scalex*px-cx, cy-scaley*py);
+			return;
+			if (!isNaN(py)) {
+				if (py > boundtop *4) {
+					ctx.lineTo(scalex*(px-cx), scaley*(cy-boundtop*4));
+					return;
+				} else if (py < boundbottom *4) {
+					ctx.lineTo(scalex*(px-cx), scaley*(cy-boundbottom*4));
+					return;
+				}
+				ctx.lineTo(scalex*(px-cx), scaley*(cy-py));
+			}
+		};
+		resize();
+	}//end init();
+	};//end ui
+	
+
+	
+	
+	
+	
+	return ui;
+})();
+
