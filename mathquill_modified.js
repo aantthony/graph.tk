8b236c9144aaf0ac0b3eb633816bf0c78829a2da
Anthony <antscape@gmail.com>
2011-01-09 23:45:00 +1100

    mathquill javascript

--- mathquill_modified.js
+++ mathquill_modified.js
@@ -9,7 +9,7 @@
 (function($){ //takes in the jQuery function as an argument
 
 //Note: if the following is no longer on line 12 of build/mathquill.js, please modify publish.sh accordingly
-//$('head').prepend('<link rel="stylesheet" type="text/css" href="http://laughinghan.github.com/mathquill/mathquill.css">');
+/*$('head').prepend('<link rel="stylesheet" type="text/css" href="http://laughinghan.github.com/mathquill/mathquill.css">');*/
 
 function todo(){ alert('BLAM!\n\nAHHHHHH!\n\n"Oh god, oh god, I\'ve never seen so much blood!"\n\nYeah, that doesn\'t fully work yet.'); };
 
@@ -2223,14 +2223,14 @@ $.fn.mathquill = function(cmd, latex)
     return createRoot.call(this, cmd);
   }
 };
-
+/*
 //on document ready, mathquill-ify all `<tag class="mathquill-*">latex</tag>`
 //elements according to their CSS class.
-/*$(function()
+$(function()
 {
   $('.mathquill-embedded-latex').mathquill();
   $('.mathquill-editable').mathquill('editable');
   $('.mathquill-textbox').mathquill('textbox');
-});*/
-
+});
+*/
 }(jQuery));
