50d22781387af2612925ac8cb89bb211389e7bf6
Anthony F <antscape@gmail.com>
2011-07-03 20:05:02 +1000

    converts to RPN

--- p.js
+++ p.js
@@ -2,293 +2,8 @@
 var eqtype={"product":1,"sum":2,"number":3,"discretevector":6,"continuousvector":7,"power":8,"fn":9,"fraction":10,"derivative":11,"integral":12,"equality":13,"pm":14,"operatorfactor":15,"lessthan":16,"greaterthan":17,"range":18};
 
 
-function p_internal(s){
-	if(!isNaN(s)){
-		return Number(s);
-	}else if(s===""){
-		return undefined;
-	}
-	var eq=[];
-	var sl=s.length;
-	var type={
-		unknown:0,
-		operator:1,
-		number:2,
-		variable:3
-	};
-	var current=type.unkown;
-	var test=[undefined,
-		"~!^&*-+=<>/",
-		"1234567890."
-	];
-	var start=0;
-	for(var i=0;i<sl;i++){
-		if(s[i]=="(" || s[i]=="["){
-			var other_end_match=s.lastIndexOf(s[i]=="["?"]":")");
-			var parenthesis=p_internal(s.substring(i+1,other_end_match));
-			eq.push(parenthesis);
-			i=other_end_match+1;
-			start=i;
-			current=undefined;
-		}
-		if(!(current && test[current].indexOf(s[i])!=-1)){
-			if(current===type.number){
-				eq.push(Number(s.substring(start,i)));
-			}else if(current===type.variable){
-				eq.push(String(s.substring(start,i)));
-			}else if(current===type.operator){
-				eq.push(s.substring(start,i));
-			}
-			start=i;
-			current=3;
-			for(var n=1;n<3;n++){
-				if(test[n].indexOf(s[i])!=-1){
-					current=n;
-					break;
-				}
-			}
-		}
-	}
-	if(start!=sl){
-		if(current===type.number){
-			eq.push(Number(s.substring(start)));
-		}else if(current===type.variable){
-			eq.push(String(s.substring(start)));
-		}else{
-			throw("Error: Expression ended in operator: "+s.substring(start))
-		}
-	}
-	//Parse operators:
-	//sum first:
-	
-	return eq;
-}
-
-function p(inp){
-	if(typeof inp==="number" || !isNaN(inp)){
-		return Number(inp);
-	}else if(typeof inp==="object"){
-		return inp;
-	}else if(inp==="" || inp===undefined){
-		return undefined;
-	}
-	
-	//cleanup routines:
-	var e=inp.replace(/\s/g,"").replace(/\]/g,")").replace(/\[/g,"(").replace(/\)\(/g,")*(");
-    
-    //TODO: known functions only, otherwise make it a product
-    //TODO: allow things like 2x
-    
-    while(e.indexOf("xx")!=-1){
-        e=e.replace(/xx/g,"x*x");
-    }
-    
-    //TODO: -- -> +
-    e=e.replace(/∞/g,"Infinity");
-    e=e.replace(/\.([^\d]|$)/g,"*$1");
-    e=e.replace(/([\d]+(\.[\d]+)?)([^\+\-\*\/\^\:\(\)\d\=\<\>\.!])/g,"$1*$3");
-    
-    //TODO: Following line is a bit hacky. Specifications need be made to clear things up.
-    e=e.replace(/([xyzπϕ])([exyzπϕ])/g,"$1*$2");
-
-	e=e.replace(/\^([\d]+)\(/g,"^$1:(");
-    
-    e=e.replace(/max\(/g,"(max)(");
-    
-    e=e.replace(/([xyzπϕ\d∫])\(/g,"$1*(");
-    e=e.replace(/\(max\)/g,"max");
-    
-    e=e.replace(/∫([^\*])/g,"∫*$1");
-    e=e.replace(/([xyzπϕ\d\.])∫/g,"$1*∫");
-	e=e.replace(/([^\+\-\*\/\^\:\(\)\d\=\<\>])\(/g,"$1:(");
-    
-	e=e.replace(/\)([^\+\-\*\/\^\:\(\)\=\<\>!])/g,")*$1");
-    //multiplicative identity
-    e=e.replace(/\*([\)\=]|$)/g,"$1");
-    //Double factorial
-    e=e.replace(/!!/g,"‼");
 
 
-	return p_internal(e);
-}
-
-
-console.log(p("1+2"));
-
-
-/*
-
-
-
-function p_internal(s){
-	if(!isNaN(s)){
-		return Number(s);
-	}else if(s===""){
-		return undefined;
-	}
-	
-	var terms=[];
-	terms.type=eqtype.sum;
-	var sl=s.length;
-	var type={
-		unknown:0,
-		operator:1,
-		number:2,
-		variable:3
-	};
-	var current=type.unknown;
-	var test=[undefined,
-		"~!^&*-+=<>/",
-		"1234567890."
-	];
-	var start=0;
-	var term=undefined;
-	var last=undefined;
-	var o   =undefined;
-	for(var i=0;i<sl;i++){
-		if(s[i]=="(" || s[i]=="["){
-			var other_end_match=s.lastIndexOf(s[i]=="["?"]":")");
-			var parenthesis=p_internal(s.substring(i+1,other_end_match));
-			last=(parenthesis);
-			i=other_end_match+1;
-			start=i;
-			current=undefined;
-		}
-		if(!(current && test[current].indexOf(s[i])!=-1)){
-			if(current===type.number){
-				last=(Number(s.substring(start,i)));
-			}else if(current===type.variable){
-				last=(String(s.substring(start,i)));
-			}else if(current===type.operator){
-				var no=s.substring(start,i);
-				console.log("reached *, term=["+last);
-				// lastlast^last*
-				if(o=="^"){
-					last=[term.pop(),last];
-					last.type=eqtype.power;
-				}else{
-					
-				}
-				
-				if(typeof term==="object"){
-					term.push(last)
-				}else{
-					term=[last];
-					term.type=eqtype.product;
-				}
-				
-				if(no=="+"){
-					while(term.length===1){
-						term=term[0];
-					}
-					terms.push(term);
-					term=undefined;
-				}else if(no=="*"){
-					
-				}else if(no=="^"){
-					
-				}
-				o=no;
-			}
-			start=i;
-			if(i<s.length){
-				current=3;
-				for(var n=1;n<3;n++){
-					if(test[n].indexOf(s[i])!=-1){
-						current=n;
-						break;
-					}
-				}
-				console.log(current,s[i])
-			}
-		}
-	}
-	if(start!=sl || 1){
-		console.log("more!",s,current);
-		if(current===type.number){
-			last=(Number(s.substring(start)));
-		}else if(current===type.variable){
-			last=(String(s.substring(start)));
-		}else if(current===undefined){
-			
-		}else{
-			throw("Error: Expression ended in operator: "+s.substring(start))
-		}
-		
-		
-		if(o=="^"){
-			last=[term.pop(),last];
-			last.type=eqtype.power;
-		}
-		
-		if(typeof term==="object"){
-			term.push(last)
-		}else{
-			term=[last];
-			term.type=eqtype.product;
-		}
-		
-		while(term.length===1){
-				term=term[0];
-		}
-		terms.push(term);
-		
-	}
-	//Parse operators:
-	//sum first:
-	console.log(["parse",s,terms.getString()]);
-	return terms;
-}
-
-function p(inp){
-	if(typeof inp==="number" || !isNaN(inp)){
-		return Number(inp);
-	}else if(typeof inp==="object"){
-		return inp;
-	}else if(inp==="" || inp===undefined){
-		return undefined;
-	}
-	
-	//cleanup routines:
-	var e=inp.replace(/\s/g,"").replace(/\]/g,")").replace(/\[/g,"(").replace(/\)\(/g,")*(");
-    
-    //TODO: known functions only, otherwise make it a product
-    //TODO: allow things like 2x
-    
-    while(e.indexOf("xx")!=-1){
-        e=e.replace(/xx/g,"x*x");
-    }
-    
-    //TODO: -- -> +
-    e=e.replace(/∞/g,"Infinity");
-    e=e.replace(/\.([^\d]|$)/g,"*$1");
-    e=e.replace(/([\d]+(\.[\d]+)?)([^\+\-\*\/\^\:\(\)\d\=\<\>\.!])/g,"$1*$3");
-    
-    //TODO: Following line is a bit hacky. Specifications need be made to clear things up.
-    e=e.replace(/([xyzπϕ])([exyzπϕ])/g,"$1*$2");
-
-	e=e.replace(/\^([\d]+)\(/g,"^$1:(");
-    
-    e=e.replace(/max\(/g,"(max)(");
-    
-    e=e.replace(/([xyzπϕ\d∫])\(/g,"$1*(");
-    e=e.replace(/\(max\)/g,"max");
-    
-    e=e.replace(/∫([^\*])/g,"∫*$1");
-    e=e.replace(/([xyzπϕ\d\.])∫/g,"$1*∫");
-	e=e.replace(/([^\+\-\*\/\^\:\(\)\d\=\<\>])\(/g,"$1:(");
-    
-	e=e.replace(/\)([^\+\-\*\/\^\:\(\)\=\<\>!])/g,")*$1");
-    //multiplicative identity
-    e=e.replace(/\*([\)\=]|$)/g,"$1");
-    //Double factorial
-    e=e.replace(/!!/g,"‼");
-
-
-	return p_internal(e);
-}
-
-*/
 String.prototype.getString=function(__ignore,javascript){
     if(javascript && window.app){
         var self=dirty(this.toString());
@@ -392,7 +107,274 @@ Array.prototype.getString=function(braces,javascript){
     }
     return s;
 };
-console.log(p("1+2*3+1+1"));
+String.prototype.__defineGetter__("s",function(){return JSON.stringify(String(this));});
+function p_internal(s){
+	var types={
+		number:1,
+		operator:2,
+		paren:3,
+		variable:4
+	};
+	var names=["none","num","op","paren","var"];
+	var current_type=0;
+	var i=0,len=s.length;
+	
+	var regex=[0,/[\d\.]/,
+	/[\!\~\^\&\*\-\+\=\/]/,/[\(\)\[\]]/,
+   /[^\!\~\^\&\*\-\+\=\/\d\.\(\)\[\]]/
+	];
+	var current_token=s[0];
+	current_type=4;
+	for(var t=1;t<4;t++){
+		if(regex[t].test(current_token)){
+			current_type=t;
+			break;
+		}
+	}
+	var output=[];
+	var stack=[];
+	function precedence(v){
+		var p=[
+			["::"],
+			["++","++","()","[]",".","->"],
+			["!","~"],
+			["*","/","%"],
+			["+","-"],
+			[">>","<<"],
+			["==","!=","!==","==="],
+			["&"],
+			["^"],
+			["|"],
+			["&&"],
+			["||"],
+			["?:"],
+			["=","+=","-=","*=","/=","%=","&=","^=","|="],
+			["throw"],
+			[","]
+			
+		];
+		for(var i=0,len=p.length;i<len;i++){
+			if(p[i].indexOf(v)!=-1){
+				return i;
+			}
+		}
+		throw("Precedence not known!");
+	}
+	
+	var left=0;
+	var right=1;
+	function associativity(v){
+		return left;
+	}
+	function next_token(token){
+		console.log(token.v.s, names[token.t]);
+		// Read a token.
+		// If the token is a number, then add it to the output queue.
+		if(token.t==types.number){
+			output.push(token);
+		}
+		// If the token is a function token, then push it onto the stack.
+		if(token.t===types.func){
+			stack.push(token);
+		}
+		// If the token is a function argument separator (e.g., a comma):
+		if(token.t===types.comma){
+			
+			// Until the token at the top of the stack is a left parenthesis,
+			while(stack[stack.length-1].v!="("){
+			
+				// If no left parentheses are encountered,
+				if(!stack.length){
+					// either the separator was misplaced or parentheses were mismatched.
+					throw("either the separator was misplaced or parentheses were mismatched.")
+				}
+				// pop operators off the stack onto the output queue.
+				output.push(stack.pop());
+			}
+			
+			
+		}
+		// If the token is an operator
+		if(token.t===types.operator){
+			//, o1, then:
+			var o1=token;
+			
+			var o1associative=associativity(o1.v);
+			var o1precedence=precedence(o1.v);
+			// ("o2" is assumed to exist)
+			var o2;
+			
+			// while
+			while(
+				
+				//there is an operator token, o2, at the top of the stack
+				(stack.length && (o2 = stack[stack.length-1]).t===types.operator)
+				//and
+				&&
+			// either
+			 (
+				(
+				//o1 is left-associative
+				o1associative==left
+				
+					//and
+					&&
+					
+				//its precedence is
+					o1precedence
+					
+					//less than or equal to
+					<=
+					
+					//that of o2,
+					precedence(o2.v)
+				)
+			//or
+			||
+				(
+				//o1 is right-associative
+				o1associative!=left
+				//and
+				&&
+				//its precedence is
+				o1precedence
+				
+				//less than
+				<
+				precedence(o2.v)
+				)
+				
+			 )
+			
+			){
+				// pop o2 off the stack, onto the output queue;
+				output.push(stack.pop());
+			}
+			
+			// push o1 onto the stack.
+			stack.push(o1);
+		}
+		
+		// If the token is a left parenthesis,
+		if(token.v=="("){
+			//then push it onto the stack.
+			stack.push(token);
+		}
+		// If the token is a right parenthesis:
+		if(token.v==")"){
+			// Until the token at the top of the stack is a left parenthesis,
+			while(stack[stack.length-1].v!="("){
+			
+				// If the stack runs out without finding a left parenthesis, then
+				if(!stack.length){
+					//there are mismatched parentheses.
+					throw("there are mismatched parentheses");
+				}
+				// pop operators off the stack onto the output queue.
+				output.push(stack.pop());
+			}
+			
+			// Pop the left parenthesis from the stack, but not onto the output queue.
+			if(stack.pop().v!="("){
+				throw("Pop the left parenthesis from the stack: Not found!")
+			}
+			
+			// If the token at the top of the stack is a function token, pop it onto the output queue.
+			if(stack[stack.length-1].t===types.func){
+				output.push(stack.pop);
+			}
+			
+		}
+		
+	};
+	
+	while(i<len){
+		i++;
+		var c;
+		if(!(i<len)){
+			next_token({v:current_token,t:current_type});
+			//send it
+			current_token="";
+			break;
+		}else if(regex[current_type].test(c=s[i])){
+			current_token+=c;
+		}else{
+			//console.log("change detected at "+s[i]);
+			//We've got a token!
+			
+			//send it
+			next_token({v:current_token,t:current_type});
+			
+			//move on
+			if(current_type===types.operator || current_type===types.paren){
+				
+				//console.log("just finished op: ",current_token.s, names[current_type]);
+				//console.log("now @ "+c);
+				//Just finished an operator.
+				
+				//send operator: Wait no, don't send it.
+				
+				current_type=4;
+				for(var t=1;t<4;t++){
+					if(regex[t].test(c)){
+						current_type=t;
+						break;
+					}
+				}
+				//console.log("just found out that ",c.s," is a "+names[current_type]);
+				
+					
+			}else{
+
+				if(regex[types.operator].test(c)){
+					//We've got an operator!
+					
+					current_type=types.operator;
+					//DO NOT SEND OPERATOR TOKEN YET.
+					
+				}else if(regex[types.paren].test(c)){
+					
+					current_type=types.paren;
+					
+				}else{
+					//Let's assume multiplication
+
+					console.error("Operator was expected between ",current_token.s," and ",c.s);
+				}
+				
+				
+				
+			}
+			current_token=c;
+			
+		}
+		
+	}
+	if(current_token.length){
+		
+		console.warn("final token: ",current_token);
+	}
+	
+	
+// When there are no more tokens to read:
+
+	// While there are still operator tokens in the stack:
+	while(stack.length){
+		var the_operator;
+		// If the operator token on the top of the stack is a parenthesis, then
+		if((the_operator=stack.pop()).t===types.paren){
+			
+			//there are mismatched parentheses.
+			throw("there are mismatched parentheses.");
+			
+		}
+		//Pop the operator onto the output queue.
+		output.push(the_operator);
+		
+	}
+	return output;
+}
+
 
 function check(n){
 	return [(p(n).getString(0,1)),(n)];
